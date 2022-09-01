154e1346b76cd39955ac14e9afcf8b682f6ae93b
narengenet <sarparast_r@yahoo.com>
2022-09-01 05:46:33 +0430

    Add Sample Controller and Repository

--- SirooWebAPP.Infrastructure/Repositories/UserRepository.cs
+++ SirooWebAPP.Infrastructure/Repositories/UserRepository.cs
@@ -23,3 +23,52 @@ namespace SirooWebAPP.Infrastructure.Repositories
         }
     }
 }
+
+
+
+
+//public class AuthorRepository
+//{
+//    List<Author> authors = new List<Author>()
+//        {
+//            new Author
+//            {
+//                Id = 1,
+//                FirstName = "Joydip",
+//                LastName = "Kanjilal"
+//            },
+//            new Author
+//            {
+//                Id = 2,
+//                FirstName = "Steve",
+//                LastName = "Smith"
+//            }
+//        };
+//    public Author GetAuthor(int id)
+//    {
+//        return authors.FirstOrDefault(a => a.Id == id);
+//    }
+//    public List<Author> GetAuthors(int pageNumber = 1)
+//    {
+//        int pageSize = 10;
+//        int skip = pageSize * (pageNumber - 1);
+//        if (authors.Count < pageSize)
+//            pageSize = authors.Count;
+//        return authors
+//          .Skip(skip)
+//          .Take(pageSize).ToList();
+//    }
+//    public bool Save(Author author)
+//    {
+//        var result = authors.Where(a => a.Id == author.Id);
+//        if (result != null)
+//        {
+//            if (result.Count() == 0)
+//            {
+//                authors.Add(author);
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
